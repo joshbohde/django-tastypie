9c168618b9a10e31637b59bb78d6d260f20958bc
Josh Bohde <josh@joshbohde.com>
2012-05-28 12:15:57 -0500

    Alter postgres calls to use postgres user instead of sudo.

--- tests/create_template_postgis-debian.sh
+++ tests/create_template_postgis-debian.sh
@@ -30,15 +30,15 @@ then
     GEOGRAPHY=1
 fi
 
-createdb -E UTF8 template_postgis && \
+createdb -u postgres -E UTF8 template_postgis && \
 ( createlang -d template_postgis -l | grep plpgsql || createlang -d template_postgis plpgsql ) && \
-psql -d postgres -c "UPDATE pg_database SET datistemplate='true' WHERE datname='template_postgis';" && \
-psql -d template_postgis -f $POSTGIS_SQL_PATH/$POSTGIS_SQL && \
-psql -d template_postgis -f $POSTGIS_SQL_PATH/spatial_ref_sys.sql && \
-psql -d template_postgis -c "GRANT ALL ON geometry_columns TO PUBLIC;" && \
-psql -d template_postgis -c "GRANT ALL ON spatial_ref_sys TO PUBLIC;"
+psql -u postgres -d postgres -c "UPDATE pg_database SET datistemplate='true' WHERE datname='template_postgis';" && \
+psql -u postgres -d template_postgis -f $POSTGIS_SQL_PATH/$POSTGIS_SQL && \
+psql -u postgres -d template_postgis -f $POSTGIS_SQL_PATH/spatial_ref_sys.sql && \
+psql -u postgres -d template_postgis -c "GRANT ALL ON geometry_columns TO PUBLIC;" && \
+psql -u postgres -d template_postgis -c "GRANT ALL ON spatial_ref_sys TO PUBLIC;"
 
 if [ $GEOGRAPHY -eq 1 ]
 then
-    psql -d template_postgis -c "GRANT ALL ON geography_columns TO PUBLIC;"
+    psql -u postgres -d template_postgis -c "GRANT ALL ON geography_columns TO PUBLIC;"
 fi
